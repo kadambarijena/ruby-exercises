02ffa757029974d98072455c060c716e26da0656
Kadambari Jena <pinkyjena1993@gmail.com>
2020-12-30 22:44:05 +0530

    arithmetic operators by using cli

---
+++ cli_add.rb
@@ -0,0 +1,4 @@
+# add two numbers reading from command line
+# ["5", "7"]
+
+puts ARGV[0].to_i + ARGV[1].to_i
---
+++ cli_sub.rb
@@ -0,0 +1,4 @@
+#subtraction two number reading from command line
+# {"a", "b"}
+
+puts ARGV[0].to_i - ARGV[1].to_i
