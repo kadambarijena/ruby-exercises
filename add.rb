484f626cd6973ca953e9e828db89fb26e6aa1d97
Kadambari Jena <pinkyjena1993@gmail.com>
2020-12-30 14:59:45 +0530

    write ruby programme how to add, sub..

---
+++ add.rb
@@ -0,0 +1,7 @@
+puts "enter first number: "
+a =   gets.to_i
+puts "Enter 2nd number:"
+b = gets.to_i
+
+puts "result : "
+puts a + b
---
+++ mult.rb
@@ -0,0 +1,11 @@
+puts "Enter 1st number"
+a = gets.to_i
+puts "Enter 2nd number"
+b = gets.to_i
+
+puts "Result:"
+
+print (a * b)
+
+
+
