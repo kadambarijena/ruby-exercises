7ccc4c2f36d0d0de75061cf6b11d7186f57fd77f
Kadambari Jena <pinkyjena1993@gmail.com>
2020-12-31 12:41:45 +0530

    Added arithmetic operands

---
+++ cli_equal.rb
@@ -0,0 +1,4 @@
+# Equal two operands reading command line
+# {"a", "b"}
+
+puts ARGV[0].to_i == ARGV[1].to_i
---
+++ cli_notequal.rb
@@ -0,0 +1,4 @@
+#notequal operands of two number reading command line
+#{"a", "b"}
+
+puts ARGV[0].to_i != ARGV[1].to_i
