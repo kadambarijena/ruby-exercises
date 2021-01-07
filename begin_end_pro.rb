c88da376cb7b9be9cac0233c1ba832b39c00a5ea
Kadambari Jena <pinkyjena1993@gmail.com>
2021-01-07 22:06:38 +0530

    1-begin-and-end-function   -Write a program for begin and end function   -Lean about initialization and terminate

---
+++ begin_end_pro.rb
@@ -0,0 +1,12 @@
+#Write a programe to boil egg
+BEGIN {
+	puts "light the gas stove"
+}
+
+puts "put eggs into a water bowl"
+puts "wait till the water boils"
+
+END {
+	puts "turn the gas stove off"
+}
+
--- begin_end_statement.rb
+++ begin_end_statement.rb
@@ -1,12 +1,12 @@
-puts "Anyone with read permissions to a repository can create a pull request, \n but you must have write permissions to create a branch"
+# to have a meal
 
-puts "You can specify which branch you'd like to merge your changes into when you create your pull request"
+puts "Enjoy the delicious meal"
 
 END {
-	puts "You can link a pull request to an issue to show that a fix is in progress and to automatically close the issue when someone merges the pull request"
+  puts "Wash your hand, after food"
+  puts "soak your hand using a towel"
 }
 
-puts "the default parent repository isn't correct"
 BEGIN {
-	puts "When you change the base repository, you also change notifications for the pull request."
+  puts "Wash your hand, before food"
 }
