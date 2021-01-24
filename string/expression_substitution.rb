d405a4c0aee9e0d96cb2bf05772f223045a8e069
Kadambari Jena <pinkyjena1993@gmail.com>
2021-01-24 23:38:25 +0530

    9-practice-ruby-strings - Write ruby program using string

---
+++ string/expression_substitution.rb
@@ -0,0 +1,5 @@
+#puts 'Won\'t you read O\'Reilly\'s book?'#using apostrophe within a single-quoted string
+x, y, z = 12, 36, 72
+puts "The value of x is #{ x }."
+puts "The sum of x and y is #{ x + y }."
+puts "The average was #{ (x + y + z)/3 }."
---
+++ string/string_built_method.rb
@@ -0,0 +1,5 @@
+#new [String.new(str = "")]
+myStr = String.new("THIS IS TEST")
+foo = myStr.downcase
+
+puts "#{foo}"
