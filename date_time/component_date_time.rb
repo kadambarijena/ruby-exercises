3cd062b99177b52e73056ead3b379185db652f18
Kadambari Jena <pinkyjena1993@gmail.com>
2021-01-25 11:26:59 +0530

    11-practiced-date-and-time - Write fuction for current date&time - simple arithmetic with date&times

---
+++ date_time/component_date_time.rb
@@ -0,0 +1,12 @@
+time = Time.new
+puts "current time : " + time.inspect
+puts time.year
+puts time.month
+puts time.hour
+puts time.min
+puts time.sec
+puts time.day
+puts time.wday
+puts time.yday
+puts time.usec
+puts time.zone
---
+++ date_time/current_date_time.rb
@@ -0,0 +1,4 @@
+time1 = Time.new
+puts "Current Time : " + time1.inspect
+time2 = Time.now
+puts "Current Time : " + time2.inspect
